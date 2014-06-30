d0b59fd30c41a8339397e66295f4b94bcce63497
lifehacker666 <lifehacker666@gmail.com>
2014-06-30 10:00:05 +0400

    first commit

---
+++ js/app/common.js
@@ -0,0 +1,26 @@
+﻿$(function(){
+
+    //активный пункт меню
+    $('.menu-top > li').click(function() {
+        $(this).parent().find('.selected').removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    /* Стартуем слайдеры */
+    if( $(".standart_slider").is(".catalog_slider") ){
+        $.getScript( '/js/lib/jquery.standart_slider.js', function() {
+
+            $('.catalog_slider').standart_slider({
+                viewport: 'viewport',
+                item: 'item',
+                timeout:12000,
+                time: 400,
+                timer:1, /* Включение-выключение перелистывания */
+                size:1 /* Количество отображаемых обьектов в окне показов */
+            });
+
+        });
+    }
+    /* /Стартуем слайдеры */
+
+});
---
+++ js/lib/jquery.standart_slider.js
@@ -0,0 +1,125 @@
+/**
+ * @author Alexandr Belikh
+ * @email zimovchanin@gmail.com
+ */
+jQuery.fn.standart_slider = function(options){
+	var options = jQuery.extend({
+		viewport: 'viewport', /* Окно показа слайдов */
+		list: 'overflower', /* Лист слайдов */
+		item: 'item', /* Сами слайды */
+		to_left: 'to_left', /* Кнопка в лево */
+		to_right: 'to_right', /* Кнопка в право */
+		show_hide_block: 'show_hide_block', /* (Блок для акций и т.п.) Открывать этот блок в случае если в слайде есть скрытое поле options.show_hide_checker со значением 1 */
+		show_hide_checker: 'show_hide_checker', /* Включатель скрытого блока */
+		buttons:'buttons', /* Блок для кнопок */
+		button:'btn', /* Кнопки */
+		selected:'selected', /* Каким классом выделять кнопки */
+		time: 400, /* Время на перелистывание в милисекундах */
+		timeout:10000, /* Время между перелистыванием */
+		timer:1, /* Включение-выключение перелистывания */
+		size:1 /* Количество отображаемых обьектов в окне показов */
+	},options);
+	
+	return this.each(function() { /* Пробегаемся по каждому слайдеру */
+		var $this = $(this);
+		var current_item = 1;
+		/* Фиксируем размер листа */
+		var $button = $this.find('.' + options.buttons + ' .' + options.button);
+		var $list = $this.find('.' + options.list);
+		var $item = $list.find('.' + options.item);
+		var item_cnt = $item.size();
+		var item_width = $item.width() + parseInt($item.css('marginRight'), 10) + parseInt($item.css('marginLeft'), 10) + parseInt($item.css('paddingRight'), 10) + parseInt($item.css('paddingLeft'), 10);
+		$list.width(item_width*(item_cnt+1));
+		/* Убираем / Показываем специальный слой */
+		var $block = $this.find('.' + options.show_hide_block);
+		var $first_block_checker = $item.first().find('.' + options.show_hide_checker);
+		if( $first_block_checker.val() == 1 ){
+			$block.show();
+		} else {
+			$block.hide();
+		}
+		/* перемотка */
+		var $left = $this.find('.' + options.to_left);
+		var $right = $this.find('.' + options.to_right);
+		
+		/* Выделяем первый */
+		$button.removeClass(options.selected);
+		$button.first().addClass(options.selected);
+		
+		$left.click(function(event){
+			if( options.timer )
+				clearInterval(cicle);
+			
+			event.preventDefault();
+			if( current_item == 1 ){
+				current_item = item_cnt-options.size+1;// Зацикливаем
+			} else {
+				current_item--; // 
+			}
+			
+			$button.removeClass(options.selected);
+			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			
+			$list.animate({left:-(current_item-1)*item_width},options.time);
+			if( $item.eq(current_item-1).find('.' + options.show_hide_checker).val() == 1 ){
+				$block.show();
+			} else {
+				$block.hide();
+			}
+			
+			if( options.timer )
+				cicle = setInterval( interval ,options.timeout);
+		});
+		$right.click(function(event){
+			if( options.timer )
+				clearInterval(cicle);
+		
+			event.preventDefault();
+			if( current_item == item_cnt-options.size+1 ){
+				current_item = 1;// Зацикливаем
+			} else {
+				current_item++; // 
+			}
+			
+			$button.removeClass(options.selected);
+			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			
+			$list.animate({left:-(current_item-1)*item_width},options.time);
+			if( $item.eq(current_item-1).find('.' + options.show_hide_checker).val() == 1 ){
+				$block.show();
+			} else {
+				$block.hide();
+			}
+			
+			if( options.timer )
+				cicle = setInterval( interval ,options.timeout);
+		});
+		
+		var interval = function(){
+			if( current_item == item_cnt-options.size+1 ){
+				current_item = 1;// Зацикливаем
+			} else {
+				current_item++; // 
+			}
+			$button.removeClass(options.selected);
+			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			$list.animate({left:-(current_item-1)*item_width},options.time);
+		};
+		
+		var cicle;
+		if( options.timer )
+			cicle = setInterval( interval ,options.timeout);
+
+		$button.click(function(){
+			if( options.timer )
+				clearInterval(cicle);
+			current_item = $button.index(this)+1;	
+			$button.removeClass(options.selected);
+			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			$list.animate({left:-(current_item-1)*item_width},options.time);
+			if( options.timer )
+				cicle = setInterval( interval ,options.timeout );
+		});
+		
+	});
+};
