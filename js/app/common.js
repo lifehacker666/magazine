5e80af9f7cb30781a7febfe0853954f5845ea53e
lifehacker666 <lifehacker666@gmail.com>
2014-07-16 11:57:16 +0400

    кнопка Наверх

--- js/app/common.js
+++ js/app/common.js
@@ -267,6 +267,24 @@
     }
     /* /scrollbar */
 
+    /* кнопка Наверх */
+    $(function() {
+        $('body').append('<div class="toTop">НАВЕРХ</div>');
+        $(window).scroll(function() {
+            if($(this).scrollTop() != 0) {
+                $('.toTop').fadeIn();
+            } else {
+                $('.toTop').fadeOut();
+            }
+        });
+
+        $('.toTop').click(function() {
+            $('body,html').animate({scrollTop:0},500);
+
+        });
+    });
+    /* /кнопка Наверх */
+
 });
 
 
