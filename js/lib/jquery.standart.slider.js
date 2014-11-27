9b62e5257fcc8e139952f2fe3a51bfa43d8437fe
lifehacker666 <lifehacker666@gmail.com>
2014-11-27 12:23:21 +0300

    add news.html, news block css fix

--- js/lib/jquery.standart.slider.js
+++ js/lib/jquery.standart.slider.js
@@ -1,4 +1,5 @@
 /**
+ * Standart Slider v1.0.1
  * @author Alexandr Belikh
  * @email zimovchanin@gmail.com
  */
@@ -20,24 +21,33 @@ jQuery.fn.standart_slider = function(options){
 		size:1, /* Количество отображаемых обьектов в окне показов */
         type: 'scroll_horiz', /* тип анимации слайдера (scroll_horiz - горизонатальная прокрутка, fade - затухание) */
         action: 'click', /*по какому событию будут срабатывать кнопки перелистывания (напр,'click, mouseover') */
-        resize: 0 /*резиновый слайдер*/
+        resize: 0, /*резиновый слайдер*/
+        buttons_list: 'boverflower', /* Лист кнопок на случай их прокрутки как слайдов */
+		bto_left: 'bto_left', /* Кнопка в лево для прокрутки кнопок */
+		bto_right: 'bto_right', /* Кнопка в право для прокрутки кнопок */
+		bsize:3, /* Количество отображаемых кнопок в окне показов */
+		buttons_scroll:false /* Прокрутка кнопок как слайдов */
 	},options);
 
 	return this.each(function() { /* Пробегаемся по каждому слайдеру */
 		var $this = $(this),
             current_item = 1,
+            bcurrent_item = 1,
             $viewport = $this.find('.' + options.viewport);
 
 		/* Фиксируем размер листа */
 		var $button = $this.find('.' + options.buttons + ' .' + options.button),
             $list = $this.find('.' + options.list),
+            $buttons_list = $this.find('.' + options.buttons_list),
             $item = $list.find('.' + options.item),
             item_cnt = $item.size(),
             sigma_indents = parseInt($item.css('marginRight'), 10) + parseInt($item.css('marginLeft'), 10) + parseInt($item.css('paddingRight'), 10) + parseInt($item.css('paddingLeft'), 10),
-            item_width = ($viewport.width() - (options.size - 1) * sigma_indents ) / (options.size);
+            item_width = ($viewport.width() - (options.size - 1) * sigma_indents ) / (options.size),
+			button_item_width = $button.width() + parseInt($button.css('marginRight'), 10) + parseInt($button.css('marginLeft'), 10) + parseInt($button.css('paddingRight'), 10) + parseInt($button.css('paddingLeft'), 10);
 
         $item.width(item_width);
         $list.width((item_width + sigma_indents)*(item_cnt+1));
+        $buttons_list.width(button_item_width*(item_cnt+1));
 
 		/* Убираем / Показываем специальный слой */
 		var $block = $this.find('.' + options.show_hide_block),
@@ -46,7 +56,6 @@ jQuery.fn.standart_slider = function(options){
         //для резинового слайдера пересчитываем величины
         if ( options.resize == 1) {
             $( window ).resize(function() {
-                //item_width = $item.width() + parseInt($item.css('marginRight'), 10) + parseInt($item.css('marginLeft'), 10) + parseInt($item.css('paddingRight'), 10) + parseInt($item.css('paddingLeft'), 10);
                 item_width = ($viewport.width() - (options.size - 1) * sigma_indents ) / (options.size);
                 $item.width(item_width);
                 $list.width((item_width + sigma_indents)*(item_cnt+1));
@@ -62,6 +71,8 @@ jQuery.fn.standart_slider = function(options){
                 $list.find('.' + options.item + '.' + options.selected).removeClass(options.selected).animate({opacity: 0}, options.time);
                 $item.eq(current_item-1).addClass(options.selected).animate({opacity: 1}, options.time);
             }
+			//console.log(bcurrent_item,button_item_width,$buttons_list);
+			$buttons_list.animate({left: -(bcurrent_item - 1) * button_item_width}, options.time);
         }
 
         animateType();
@@ -78,6 +89,8 @@ jQuery.fn.standart_slider = function(options){
 		/* перемотка */
 		var $left = $this.find('.' + options.to_left);
 		var $right = $this.find('.' + options.to_right);
+		var $bleft = $this.find('.' + options.bto_left);
+		var $bright = $this.find('.' + options.bto_right);
 		
 		/* Выделяем первый */
 		$button.removeClass(options.selected);
@@ -88,14 +101,20 @@ jQuery.fn.standart_slider = function(options){
 				clearInterval(cicle);
 			
 			event.preventDefault();
-			if( current_item == 1 ){
+			if( current_item <= 1 ){
 				current_item = item_cnt-options.size+1;// Зацикливаем
 			} else {
-				current_item--; // 
+				current_item--; //
 			}
+			bcurrent_item = current_item;
+			//if( bcurrent_item == 1 ){
+			//	bcurrent_item = item_cnt-options.bsize+1;// Зацикливаем
+			//} else {
+			//	bcurrent_item--; //
+			//}
 			
 			$button.removeClass(options.selected);
-			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			jQuery($button.eq( Math.ceil((current_item - 1)/options.size)) ).addClass(options.selected);
 
             animateType();
 
@@ -113,14 +132,20 @@ jQuery.fn.standart_slider = function(options){
 				clearInterval(cicle);
 		
 			event.preventDefault();
-			if( current_item == item_cnt-options.size+1 ){
+			if( current_item >= item_cnt-options.size+1 ){
 				current_item = 1;// Зацикливаем
 			} else {
 				current_item++; // 
 			}
+			bcurrent_item = current_item;
+			//if( bcurrent_item == item_cnt-options.bsize+1 ){
+			//	bcurrent_item = 1;// Зацикливаем
+			//} else {
+			//	bcurrent_item++; // 
+			//}
 			
 			$button.removeClass(options.selected);
-			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			jQuery($button.eq( Math.ceil((current_item - 1)/options.size)) ).addClass(options.selected);
 
             animateType();
 
@@ -134,14 +159,53 @@ jQuery.fn.standart_slider = function(options){
 				cicle = setInterval( interval ,options.timeout);
 		});
 		
+		$bleft.click(function(event){
+			if( options.timer )
+				clearInterval(cicle);
+			
+			event.preventDefault();
+			if( bcurrent_item == 1 ){
+				bcurrent_item = item_cnt-options.bsize+1;// Зацикливаем
+			} else {
+				bcurrent_item--; //
+			}
+
+            animateType();
+			
+			if( options.timer )
+				cicle = setInterval( interval ,options.timeout);
+		});
+		$bright.click(function(event){
+			if( options.timer )
+				clearInterval(cicle);
+		
+			event.preventDefault();
+			if( bcurrent_item == item_cnt-options.bsize+1 ){
+				bcurrent_item = 1;// Зацикливаем
+			} else {
+				bcurrent_item++; // 
+			}
+
+            animateType();
+			
+			if( options.timer )
+				cicle = setInterval( interval ,options.timeout);
+		});
+		
 		var interval = function(){
-			if( current_item == item_cnt-options.size+1 ){
+			if( current_item >= item_cnt-options.size+1 ){
 				current_item = 1;// Зацикливаем
 			} else {
 				current_item++; // 
 			}
+			bcurrent_item = current_item;
+			//if( bcurrent_item == item_cnt-options.bsize+1 ){
+			//	bcurrent_item = 1;// Зацикливаем
+			//} else {
+			//	bcurrent_item++; // 
+			//}
 			$button.removeClass(options.selected);
-			jQuery($button.eq(current_item-1)).addClass(options.selected);
+			jQuery($button.eq( Math.ceil((current_item - 1)/options.size)) ).addClass(options.selected);
 
             animateType();
 		};
@@ -153,9 +217,16 @@ jQuery.fn.standart_slider = function(options){
         $button.bind(options.action, function(e){
             if( options.timer )
                 clearInterval(cicle);
-            current_item = $button.index(this)+1;
+
+            current_item = $button.index(this)*options.size+1;
+
+            // Случай, когда перематывать элт-ы нужноне полностью (Если колв-во эл-тов > чем помещается в видимой части слайдера)
+            if( current_item > (item_cnt - options.size) ) {
+                current_item = item_cnt - options.size + 1;
+            }
+
             $button.removeClass(options.selected);
-            jQuery($button.eq(current_item-1)).addClass(options.selected);
+            jQuery($button.eq($button.index(this) )).addClass(options.selected);
 
             $list.stop();
             animateType();
